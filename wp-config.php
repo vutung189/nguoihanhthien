586ebdc1ecd85c1e709bb48a6e3a3f00dae6f0ba
Vu Tung <vutung1891995@gmail.com>
2018-03-26 20:58:09 +0700

    dh

--- wp-config.php
+++ wp-config.php
@@ -23,22 +23,20 @@
 
 
 // ** MySQL settings - You can get this info from your web host ** //
-$url = parse_url(getenv('DATABASE_URL') ? getenv('DATABASE_URL') : getenv('CLEARDB_DATABASE_URL'));
-
 /** The name of the database for WordPress */
-define('DB_NAME', trim($url['path'], '/'));
+define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', $url['user']);
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', $url['pass']);
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
